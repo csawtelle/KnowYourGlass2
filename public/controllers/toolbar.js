0005ace3d8bd67cde44d530c2b08a6c479a5cb78
Izadien <csawtelle@live.com>
2016-09-27 19:51:41 -0700

    Fix up toolbar, add new colors and fix index, latest post

--- public/controllers/toolbar.js
+++ public/controllers/toolbar.js
@@ -4,3 +4,14 @@ angular.module('app').controller('toolbarCtrl', ['$scope', '$rootScope', '$http'
 angular.module('app').controller('latestPostsCtrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
 
 }]);
+
+angular.module('app').controller('TabController', function(){
+    this.tab = 0;
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+});
