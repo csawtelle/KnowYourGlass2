cf0bf62f6a7e916fe13bda08d6bc23b4e97ae995
csawtelle <csawtelle@live.com>
2016-10-12 20:11:26 -0700

    Angular2

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic'
 
 @Component({
   selector: 'my-app',
   templateUrl: 'views/home.html'
 })
 export class AppComponent { }
-bootstrap(AppComponent)
