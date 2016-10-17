0cf45c972b876583189c42417597adff5189a41d
csawtelle <csawtelle@live.com>
2016-10-16 22:46:53 -0700

    Customize modal a little

--- public/app/edit-review.component.ts
+++ public/app/edit-review.component.ts
@@ -1,8 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Review } from './models/review';
+import { ReviewService } from './review.service';
 
 @Component({
   selector: 'kyg-app',
   templateUrl: 'views/edit.html',
 })
 export class EditComponent { 
+
+  title = "Know Your Glass";
+  reviews: Review[];
+  sreview: Review[];
+
+  constructor(private reviewService: ReviewService) { 
+    this.reviewService.reviews.subscribe((observer) => {
+      console.log(JSON.parse(observer._body));
+      this.reviews = JSON.parse(observer._body).data;
+    });
+  }
 }
