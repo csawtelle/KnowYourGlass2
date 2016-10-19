a2f5d2513094a3a736c68bd2a7448c9b8231c54c
gramos <gerard.ramosm89@gmail.com>
2016-10-19 10:29:01 -0700

    committing changes before changing the output directories for the js files from ts

--- public/app/app-routing.module.ts
+++ public/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { EditComponent }      from './edit-review.component';
 import { ReviewComponent }      from './review.component';
 import { AdminComponent }  from './admin.component';
 import { PageNotFoundComponent }  from './pagenotfound.component';
-
+import { LoginComponent } from './login.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'admin', component: EditComponent },
   { path: 'review', component: ReviewComponent },
   { path: 'review/:id', component: ReviewComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**',     component: PageNotFoundComponent }
 ];
 @NgModule({
