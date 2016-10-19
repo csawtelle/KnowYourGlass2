d6df866db6dbdfe2779d600824e79661644149de
csawtelle <csawtelle@live.com>
2016-10-19 12:57:42 -0700

    Fix more broken-ness with RXJS import

--- public/app/main.ts
+++ public/app/main.ts
@@ -1,5 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import { ReviewService } from './review.service';
+import 'rxjs/Rx';
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule, [ReviewService]);
