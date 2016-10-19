e9587adb8f746d80fb736a4c926bdf375aa08926
csawtelle <csawtelle@live.com>
2016-10-19 11:55:12 -0700

    Add rxjs components

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'kyg-app',
   templateUrl: 'views/toolbar.html',
--- public/app/review.component.ts
+++ public/app/review.component.ts
@@ -1,26 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Review } from './models/review';
 import { ReviewService } from './review.service';
-
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'kyg-app',
+  selector: 'review',
   templateUrl: 'views/review.html',
 })
 export class ReviewComponent { 
-
-  title = "Know Your Glass";
+  review: Review[];
   reviewName = '';
-  reviews: Review[];
 
   constructor(
     private reviewService: ReviewService,
     private route: ActivatedRoute,
-    private router: Router) { 
-      this.reviewName = this.route.params._value.id
-      this.reviewService.reviews.subscribe((observer) => {
-        this.reviews = JSON.parse(observer._body).data;
-      });
-    }
+    private router: Router) { }
+
+  ngOnInit() {
+    this.reviewName = this.route.params._value.id;
+    this.reviewService.getReview(this.reviewName).subscribe(review => this.review = review[0]);
+  }
 }
