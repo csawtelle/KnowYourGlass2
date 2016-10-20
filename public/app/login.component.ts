c92954c0d6041ce05c77071ea82ef2c5bd8b367e
gramos <gerard.ramosm89@gmail.com>
2016-10-20 08:42:38 -0700

    testing

--- public/app/login.component.ts
+++ public/app/login.component.ts
@@ -2,5 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: 'views/login.html',
+  styles: [`
+
+  .jumbotron { 
+    box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2);
+    background-color: #4c5a6f;
+    color:white;
+   }
+  `]
 })
 export class LoginComponent {}
