0752342d7af8d9f4ead6b65164e3959418385ae7
csawtelle <csawtelle@live.com>
2016-10-25 16:13:09 -0700

    Code cleanup

--- public/app/app-routing.module.ts
+++ public/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
-import { EditComponent } from './edit-review.component';
 import { ReviewComponent } from './review.component';
 import { AdminComponent }  from './admin.component';
 import { PageNotFoundComponent }  from './pagenotfound.component';
