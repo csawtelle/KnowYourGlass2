f59e0126bdc1683e6b783ca140eae0fe1c5f1c7b
gramos <gerard.ramosm89@gmail.com>
2016-10-27 16:13:32 -0700

    user model

---
+++ public/app/models/user.interface.ts
@@ -0,0 +1,6 @@
+
+export interface User {
+    username: string; // required, must be 5-8 characters
+    password: string; // required, value must be equal to confirm password.
+    confirmPassword: string; // required, value must be equal to password.
+}
