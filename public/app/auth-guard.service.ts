1d73b7677a4e0b6d1c154ea1f23aec71b4ae0e47
gramos <gerard.ramosm89@gmail.com>
2016-10-31 08:47:04 -0700

    admin page can check if user is logged in need to get middleware to check validity of user logged in

--- public/app/auth-guard.service.ts
+++ public/app/auth-guard.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Routes, Router } from '@angular/router';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   canActivate(){
-    console.log('Checking right now to see if the user is logged in');
-    return true;
+    //console.log('Checking right now to see if the user is logged in');
+     
+    return this.checkLogin()
 
+  }
+
+  checkLogin(){
+    if (true) { 
+      return true 
+    }
+    console.log("Checking if user is logged in.");
+  
   }
 }
