e04fc9c783ec53e7fceb30493a73967021b0b21d
gramos <gerard.ramosm89@gmail.com>
2016-11-09 15:44:25 -0800

    commiting changes

--- public/app/mdriven-login.component.ts
+++ public/app/mdriven-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component ({ 
   selector: 'mdriven-login',
@@ -7,11 +8,34 @@ import { Component, OnInit } from '@angular/core';
 
 export class ModelDrivenLogin implements OnInit {
   
-
-  constructor(){}
+  form: FormGroup;
+  usernameErr: string;
+  passErr: string;
+ constructor(private fb: FormBuilder){}
   
   ngOnInit(){
     //form is built here
-    
+    this.form = this.fb.group({
+      username: [''],
+      password: ['']
+    });
+
+    //watch for changes as we validate
+    this.form.valueChanges.subscribe(data => { console.log(data); });
+
+    //validate each field
+    let username = this.form.get('username');
+    let password = this.form.get('password');
+    if (username.dirty){
+      this.usernameErr = "Please enter a username";
+    }
+    if (password.invalid && password.dirty){
+      this.passErr = "Please enter a password";
+    }
+
+    console.log(this.form);
+  }
+  processForm(){
+    console.log("submit button was clicked", this.form.value);
   }
 }
