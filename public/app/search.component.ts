6227bbc96d9ff98ca79e94f48fe8b2d22dc0c266
csawtelle <csawtelle@live.com>
2016-11-09 16:46:22 -0800

    Increase debounce time

--- public/app/search.component.ts
+++ public/app/search.component.ts
@@ -17,7 +17,7 @@ export class SearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.reviews = this.searchTerms
-      .debounceTime(300)
+      .debounceTime(700)
       .distinctUntilChanged()
       .switchMap(term => term ? this.reviewService.reviewSearch(term): Observable.of<Review[]>([]))
       .catch(error => {
