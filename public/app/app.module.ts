fac59ca2aceb1472311f6ce8cea3dad5286f9236
gramos <gerard.ramosm89@gmail.com>
2016-11-10 08:49:02 -0800

    mobaxterm bug putting g's at the beginning

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -1,4 +1,4 @@
-gmport { NgModule }      from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule }   from '@angular/router';
