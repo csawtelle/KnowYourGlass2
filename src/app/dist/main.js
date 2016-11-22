960b4e3e929327ab866b0f1711838934cbc5f41a
csawtelle <csawtelle@live.com>
2016-11-22 13:07:58 -0800

    Remove dist

--- src/app/dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-var review_service_1 = require('./review.service');
-require('rxjs/Rx');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule, [review_service_1.ReviewService]);
-//# sourceMappingURL=main.js.map
