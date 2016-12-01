a2f592ac282a5376624c0dbac8b600f052fde35f
csawtelle <csawtelle@live.com>
2016-11-30 23:46:28 -0800

    File uploader actually added

---
+++ src/app/uploader.component.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+
+// const URL = '/api/';
+const URL = 'https://knowyourglass.com/api/upload';
+
+@Component({
+  selector: 'file-upload',
+  templateUrl: './views/uploader.html',
+})
+export class UploaderComponent {
+  public uploader:FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver:boolean = false;
+  public hasAnotherDropZoneOver:boolean = false;
+
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public fileOverAnother(e:any):void {
+    this.hasAnotherDropZoneOver = e;
+  }
+}
