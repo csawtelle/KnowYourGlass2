08a34acc37cf215d50fe0eb757e99fb04aa17451
csawtelle <csawtelle@live.com>
2016-11-30 23:46:08 -0800

    File upload added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule }   from '@angular/router';
 import { HttpModule }    from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent }   from './app.component';
 import { HomeComponent }   from './home.component';
 import { ReviewComponent }   from './review.component';
 import { AdminComponent }   from './admin.component';
 import { PageNotFoundComponent }   from './pagenotfound.component';
 import { LoginComponent } from './login.component';
+import { SearchComponent } from './search.component';
+import { SearchBarComponent } from './search-bar.component';
+import { UploaderComponent } from './uploader.component';
+
 import { ReviewService } from './review.service';
+
 import { AppRouter } from './app-routing.module';
 import { Review } from './models/review';
 import { AuthGuard } from './auth-guard.service';
 import { AuthService } from './auth.service';
 import { KeysPipe } from './filters/keys.pipe';
-
-import { SearchComponent } from './search.component';
-import { SearchBarComponent } from './search-bar.component';
 import { CKEditorModule } from 'ng2-ckeditor';
+import { FileUploadModule } from 'ng2-file-upload';
+
 
 @NgModule({
   imports:  [ 
@@ -30,6 +35,7 @@ import { CKEditorModule } from 'ng2-ckeditor';
     HttpModule,
     NgbModule.forRoot(),
     CKEditorModule,
+    FileUploadModule,
     ReactiveFormsModule 
   ],
 
@@ -42,6 +48,7 @@ import { CKEditorModule } from 'ng2-ckeditor';
     LoginComponent,
     SearchComponent,
     SearchBarComponent,
+    UploaderComponent,
     KeysPipe
  ],
 
