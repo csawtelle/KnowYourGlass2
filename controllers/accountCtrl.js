d6f471a15c487d6ad0ccf93af4da762cd5b4cbb7
Chris Sawtelle <csawtelle@live.com>
2017-02-09 15:33:18 -0800

    76% through email verification

---
+++ controllers/accountCtrl.js
@@ -0,0 +1,95 @@
+var nodemailer = require('nodemailer');
+var generator = require('generate-password');
+var User = require('../models/user');
+var TempUser = require('../models/tempUser');
+
+exports.createAccount = function(req, res) {
+  var tempPassword = generator.generate({ length: 20, numbers: true });
+  var email = req.body.email;
+  var user = new TempUser({
+    username: req.body.username,
+    password: tempPassword,
+    email: email
+  });
+
+  user.save(function(err) {
+    if (err) throw err;
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+
+  var transporter = nodemailer.createTransport({
+	  host: 'mail.privateemail.com',
+		port: '465',
+		secure: true,
+		auth: {
+	    user: "admin@knowyourglass.com",
+			pass: "KYG@99"
+		},
+    tls: {
+        rejectUnauthorized: false
+    }
+  });
+  transporter.sendMail({
+			from: '"Know Your Glass" <admin@knowyourglass.com>',
+			to: email,
+      bcc: 'admin@knowyourglass.com',
+			subject: 'Please verify your registration.',
+			html: '<p>Please login using your temporary password: ' + tempPassword + '</p>',
+      text: 'Please login using your temporary password: ' + tempPassword + '</p>'
+	});
+  transporter.close();
+};
+
+exports.verifyAccount = function(req, res) {
+  var username = req.body.username;
+  var email = req.body.email;
+  var tempPassword = req.body.tempPassword;
+  var persistPassword = req.body.persistPassword;
+
+  TempUser.findOne({
+    username: username
+  }, function(err, user) {
+	  if (err) { return callback(err); }
+		if (!user) {
+      console.log("Temporary User not found.");
+      res.json({ success: false, message: 'Temporary User not found' });
+    }	
+		user.verifyPassword(tempPassword, function(err, isMatch) {
+			if (err) { 
+				console.log('Compare password function failed. System Error.');
+				res.json({ success: false, message: 'User not found.' });
+			}
+			if (!isMatch) { 
+				console.log('Incorrect password. Authentication Error.');
+				res.json({ success: false, message: 'Incorrect password.' });
+			}
+			if (isMatch) { 
+
+        var user = new User({
+          username: username,
+          password: persistPassword,
+          email: email
+        });
+
+        user.save(function(err) {
+          if (err) throw err;
+          console.log('User saved successfully');
+          res.json({ success: true });
+        });
+
+				console.log("User authentication succeeded.");
+				var token = jwt.sign(user, 'superSecret', {
+					expiresIn: 1440 // expires in 24 hours
+				});
+				res.json({
+					success: true,
+					message: 'User authentication succeeded.',
+					token: token
+				});
+			}
+	  });
+  });
+}
+
+
---
+++ models/tempUser.js
@@ -0,0 +1,53 @@
+// Load required packages
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+// Define our user schema
+var TempUserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+    expires: 300
+});
+
+// Execute before each user.save() call
+TempUserSchema.pre('save', function(callback) {
+  var user = this;
+
+  // Break out if the password hasn't changed
+  if (!user.isModified('password')) return callback();
+
+  // Password changed so we need to hash it
+  bcrypt.genSalt(5, function(err, salt) {
+    if (err) return callback(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return callback(err);
+      user.password = hash;
+      callback();
+    });
+  });
+});
+
+TempUserSchema.methods.verifyPassword = function(password, cb) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+// Export the Mongoose model
+module.exports = mongoose.model('TempUser', TempUserSchema);
