05f2c2e9732907286ff5aec6fdb798931e4cdd7e
Chris Sawtelle <csawtelle@live.com>
2017-02-09 14:18:32 -0800

    Fix login page to allow registration

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -12,6 +12,8 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
   usernameErr: string;
   passErr: string;
+  emailErr: string;
+  registered: boolean = true;
   public response: any;
   public token: any;
  constructor(public tokenService: TokenService, private router: Router, private fb: FormBuilder, public authService: AuthService){
@@ -21,25 +23,40 @@ export class LoginComponent implements OnInit {
     //form is built here
     this.form = this.fb.group({
       username: [''],
-      password: ['']
+      password: [''],
+      email: ['']
     });
     //watch for changes as we validate
     this.form.valueChanges.subscribe(data => { 
-    let username = this.form.get('username');
-    let password = this.form.get('password');
-    if (username.invalid && username.dirty){
-      this.usernameErr = "Please enter a username";
-    } else {
-      this.usernameErr = null;
-    }
-    if (password.invalid && password.dirty){
-      this.passErr = "Please enter a password";
-    } else {
-      this.passErr = null;
-    }
-   
-   
+      let username = this.form.get('username');
+      let password = this.form.get('password');
+      let email = this.form.get('email');
+      if (username.invalid && username.dirty){
+        this.usernameErr = "Please enter a username";
+      } else {
+        this.usernameErr = null;
+      }
+      if (password.invalid && password.dirty){
+        this.passErr = "Please enter a password";
+      } else {
+        this.passErr = null;
+      }
+       if (email.invalid && email.dirty){
+        this.emailErr = "Please enter an Email";
+      } else {
+        this.emailErr = null;
+      }
     });
+  }
+
+  swapForm(registered: boolean){
+    console.log(registered);
+    console.log(this.registered);
+    if(registered) {
+      this.registered = false;
+    } else {
+      this.registered = true;
+    }
   }
   processForm(){
     this.token = "Logging in...";
@@ -47,7 +64,6 @@ export class LoginComponent implements OnInit {
     this.tokenService.loginDelay().subscribe(() => {
       if (this.tokenService.currentToken) {
         this.token = "Login Success! Redirecting";
-        
         this.router.navigate(['/admin']);
       }
     }); 
