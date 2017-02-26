be6fb80579757bcdec4db8bb27a194868939b80b
KnowYourGlass <gerard.ramosm89@gmail.com>
2017-02-26 13:38:55 -0800

    shows on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,18 @@ import { AuthService } from './auth.service';
   templateUrl: './views/app.html',
 })
 export class AppComponent { 
-  
+  show: boolean = false;
   constructor( private authService: AuthService ) {
     if (localStorage.getItem('user')) {
       this.authService.validateToken().subscribe();
     }
   }
+  toggleMenu(){
+    this.show = !this.show;
+    console.log("Toggle menu was clicked");
+  }
   logout(){
     this.authService.logout();
+    console.log("Logout was clicked");
   }
 }
